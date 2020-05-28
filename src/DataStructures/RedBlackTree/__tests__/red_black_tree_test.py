35fae0d54a80089fae36a25330b631fe45fee881
nalhin <krol.wow@gmail.com>
2020-05-28 16:05:05 +0200

    feat: RBT insert operation.

--- src/DataStructures/RedBlackTree/__tests__/red_black_tree_test.py
+++ src/DataStructures/RedBlackTree/__tests__/red_black_tree_test.py
@@ -1,10 +1,33 @@
+from random import random
+
 import pytest
+
+from src.DataStructures.RedBlackTree.red_black_tree import RedBlackTree
 
 
 @pytest.fixture()
 def red_black_tree():
-    return RedBlackTreeTest()
+    return RedBlackTree()
 
 
-class RedBlackTreeTest:
-    pass
+def populate_tree(binary_search_tree, items):
+    for i in items:
+        binary_search_tree.insert(i)
+
+
+class TestRedBlackTree:
+    def test_insert_predefined_values(self, red_black_tree):
+        values = [3, 2, 12, 4, 5, 6, 2]
+
+        populate_tree(red_black_tree, values)
+        result = red_black_tree.inorder_tree_traversal()
+
+        assert result == sorted(values)
+
+    def test_insert_random_values(self, red_black_tree):
+        values = [random() for _ in range(100)]
+
+        populate_tree(red_black_tree, values)
+        result = red_black_tree.inorder_tree_traversal()
+
+        assert result == sorted(values)
--- src/DataStructures/RedBlackTree/red_black_tree.py
+++ src/DataStructures/RedBlackTree/red_black_tree.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import List
 
 
 class Color(Enum):
@@ -15,5 +16,110 @@ class Node:
         self.parent = parent
 
 
+NULL = Node(None, Color.BLACK)
+
+
 class RedBlackTree:
-    pass
+    def __init__(self):
+        self.root = NULL
+
+    def inorder_tree_traversal(self) -> List:
+        """returns items inserted into the tree in sorted order"""
+        items = []
+
+        self._recursive_tree_traversal(self.root, items)
+
+        return items
+
+    def _recursive_tree_traversal(self, node, items):
+        """traversals the tree and appends values in sorted order"""
+        if node == NULL:
+            return
+
+        self._recursive_tree_traversal(node.left, items)
+        items.append(node.val)
+        self._recursive_tree_traversal(node.right, items)
+
+    def _left_rotate(self, rotated):
+        to_rotate = rotated.left
+        rotated.right = to_rotate.left
+
+        if rotated.left != NULL:
+            rotated.left.parent = rotated
+
+        if to_rotate.parent == NULL:
+            self.root = rotated
+        elif to_rotate.parent.left == to_rotate:
+            to_rotate.parent.left = rotated
+        else:
+            to_rotate.parent.right = rotated
+
+        rotated.left = to_rotate
+        to_rotate.parent = rotated
+
+    def _right_rotate(self, rotated):
+        to_rotate = rotated.left
+        to_rotate.left = rotated.right
+
+        if rotated.right != NULL:
+            rotated.right.parent = rotated
+
+        if rotated.parent == NULL:
+            self.root = rotated
+        elif rotated.parent.left == to_rotate:
+            rotated.parent.left = rotated
+        else:
+            rotated.parent.right = rotated
+
+        rotated.left = to_rotate
+        to_rotate.parent = rotated
+
+    def insert(self, val):
+        node = self.root
+        parent = NULL
+
+        while node != NULL:
+            parent = node
+            if node.val > val:
+                node = node.left
+            else:
+                node = node.right
+
+        new_node = Node(val, Color.RED, left=NULL, right=NULL, parent=parent)
+
+        if parent == NULL:
+            self.root = new_node
+        elif parent.val > new_node.val:
+            parent.left = new_node
+        else:
+            parent.right = new_node
+
+    def _insert_fixup(self, node):
+        while node.parent.color == Color.RED:
+            grand_parent = node.parent.parent
+            if node.parent == grand_parent.left:
+                if grand_parent.right.color == Color.RED:
+                    node.parent.color = Color.BLACK
+                    grand_parent.right.color = Color.BLACK
+                    grand_parent.color = Color.BLACK
+                elif node == node.parent.right:
+                    node = node.parent
+                    self._left_rotate(node)
+
+                node.parent.color = Color.BLACK
+                grand_parent.color = Color.RED
+                self._right_rotate(grand_parent)
+            else:
+                if grand_parent.left.color == Color.RED:
+                    node.parent.color = Color.BLACK
+                    grand_parent.left.color = Color.BLACK
+                    grand_parent.color = Color.BLACK
+                elif node == node.parent.left:
+                    node = node.parent
+                    self._right_rotate(node)
+
+                node.parent.color = Color.BLACK
+                grand_parent.color = Color.RED
+                self._left_rotate(grand_parent)
+
+        self.root = Color.BLACK
